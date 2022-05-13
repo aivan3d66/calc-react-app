edcc3ceed1e6590e36bb1f8fbcfe4412b2da1e8e
Ivan <adamovskij00@mail.ru>
2022-05-13 17:05:14 +0300

    add Settings props

--- src/components/Settings/index.jsx
+++ src/components/Settings/index.jsx
@@ -5,16 +5,18 @@ import Button from '@/components/Button'
 import { useDispatch, useSelector } from 'react-redux'
 import { actions } from '@/actions'
 import { themesOptions } from '@/constants'
+import theme from '@/theme'
 
 export default () => {
   const dispatch = useDispatch()
   const { themeValue } = useSelector(state => state.appReducer)
+  const schema = themeValue === 'Light theme' ? theme.appLightTheme : themeValue === 'Colored theme' ? theme.colourTheme : theme.appDarkTheme
 
   const onBtnHandler = () => dispatch(actions.clearHistory())
   const onSelectHandler = value => dispatch(actions.setToggleTheme(value))
 
   return (
-    <Settings id="settingsContainer">
+    <Settings id="settingsContainer" schema={schema}>
       <Heading id="settings">Settings</Heading>
       <SelectHeading>Switch theme</SelectHeading>
       <Select
