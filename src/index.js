d60f9b047cbf470cf852f78721e3295ae4fe2213
Ivan <adamovskij00@mail.ru>
2022-05-14 16:01:50 +0300

    add fix store import

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-
 import React from "react"
 import ReactDOM from "react-dom"
-
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { ThemeProvider } from "styled-components"
-
 import Application from "@/App"
-
-import { store } from "@/store"
 import theme from "@/theme"
 import GlobalStyles from "@/globalStyles"
+import store from "@/store"
 
 ReactDOM.render(
   <Provider store={store}>
