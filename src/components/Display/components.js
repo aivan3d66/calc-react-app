8c5b66ca590f57d5f7c1aca6546621d34ffe6ee6
Ivan <adamovskij00@mail.ru>
2022-05-14 22:06:46 +0300

    add Display grid-gap style

--- src/components/Display/components.js
+++ src/components/Display/components.js
@@ -4,6 +4,7 @@ export const Display = styled.div`
   display: grid;
   grid-template-columns: 1fr auto;
   grid-template-rows: 1fr 1fr;
+  grid-gap: 20px;
   max-width: 1280px;
   max-height: 900px;
   margin: 0 auto;
