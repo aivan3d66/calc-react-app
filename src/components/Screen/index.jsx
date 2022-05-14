cabed0841f329db3af308f84f1d1b89b82f524d1
Ivan <adamovskij00@mail.ru>
2022-05-14 16:02:19 +0300

    add Screen errorString handler

--- src/components/Screen/index.jsx
+++ src/components/Screen/index.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
-import { Screen } from '@/components/Screen/components'
+import { Error, Screen } from '@/components/Screen/components'
 import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import theme from '@/theme'
 
-export default ({ screenValue }) => {
+export default ({ screenValue, errorString }) => {
   const { themeValue } = useSelector(state => state.appReducer)
   const schema = themeValue === 'Light theme' ? theme.appLightTheme : themeValue === 'Colored theme' ? theme.colourTheme : theme.appDarkTheme
 
   return (
     <Screen schema={schema} id="screenContainer">
-      {screenValue}
+      {errorString ? <Error>{errorString}</Error> : screenValue}
     </Screen>
   )
 }
 
 Screen.propTypes = {
   screenValue: PropTypes.number,
+  errorString: PropTypes.string,
 }
