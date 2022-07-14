253200925e1d04160341c91d37516a67e48a3c5e
Ivan <adamovskij00@mail.ru>
2022-07-14 09:42:04 +0300

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { ThemeProvider } from "styled-components"
 import Application from "@/App"
@@ -12,10 +12,10 @@ import store from "@/store"
 ReactDOM.render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <HashRouter>
         <Application />
         <GlobalStyles />
-      </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </Provider>,
   document.getElementById("root"),
