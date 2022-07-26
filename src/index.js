84f76a77d67a4a4ab5f44e2c98445a02ffbb15ce
Ivan <adamovskij00@mail.ru>
2022-07-26 08:08:01 +0300

    add upd ReactDOM render

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import React from "react"
 import ReactDOM from "react-dom"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
-import { ThemeProvider } from "styled-components"
 
-import Application from "@/App"
-import theme from "@/theme"
 import GlobalStyles from "@/globalStyles"
 import store from "@/store"
+import { App } from "@/App"
+import { ErrorBoundary } from "@/components/ErrorBoundary"
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <HashRouter>
-        <Application />
-        <GlobalStyles />
-      </HashRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <ErrorBoundary>
+          <App />
+          <GlobalStyles />
+        </ErrorBoundary>
+      </BrowserRouter>
     </ThemeProvider>
   </Provider>,
   document.getElementById("root"),
