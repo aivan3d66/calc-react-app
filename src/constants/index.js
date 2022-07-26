eced367bd1de0b7179e2e94b2ee59580ec8c6fef
Ivan <adamovskij00@mail.ru>
2022-07-26 20:09:16 +0300

    add buttonValues

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,94 +1,123 @@
-import { ACTIONS } from '@/constants/actions'
-
 export * from './actions'
 export * from './router'
 
 export const themesOptions = ['Light theme', 'Colored theme', 'Dark theme']
 
-export const btnValues = [
-  {
-    value: 'C',
-    dispatchType: ACTIONS.CLEAR,
-  },
-  {
-    value: '7',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '8',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '9',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '*',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
+// export const btnValues = [
+//   {
+//     value: 'C',
+//     dispatchType: CLEAR,
+//   },
+//   {
+//     value: '7',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '8',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '9',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '*',
+//     dispatchType: ADD_VALUE,
+//   },
+//
+//   {
+//     value: '-',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '4',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '5',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '6',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '/',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '+',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '1',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '2',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '3',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '=',
+//     dispatchType: EVALUATE,
+//   },
+//   {
+//     value: '.',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '(',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: '0',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: ')',
+//     dispatchType: ADD_VALUE,
+//   },
+//   {
+//     value: 'CE',
+//     dispatchType: DELETE_VALUE,
+//   },
+//   {
+//     value: '%',
+//     dispatchType: ADD_VALUE,
+//   },
+// ]
 
-  {
-    value: '-',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '4',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '5',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '6',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '/',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '+',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '1',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '2',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '3',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '=',
-    dispatchType: ACTIONS.EVALUATE,
-  },
-  {
-    value: '.',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '(',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: '0',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: ')',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
-  {
-    value: 'CE',
-    dispatchType: ACTIONS.DELETE_VALUE,
-  },
-  {
-    value: '%',
-    dispatchType: ACTIONS.ADD_VALUE,
-  },
+export const buttonValues = [
+  [
+    { value: 'C' },
+    { value: '7' },
+    { value: '8' },
+    { value: '9' },
+    { value: '*' },
+  ],
+  [
+    { value: '-' },
+    { value: '4' },
+    { value: '5' },
+    { value: '6' },
+    { value: '/' },
+  ],
+  [
+    { value: '+' },
+    { value: '1' },
+    { value: '2' },
+    { value: '3' },
+    { value: '=' },
+  ],
+  [
+    { value: '.' },
+    { value: '%' },
+    { value: '0' },
+    { value: '+-' },
+    { value: 'CE' },
+  ],
 ]
