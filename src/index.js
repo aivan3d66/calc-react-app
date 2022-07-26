e4cf4b2491ec88ffd638a9f7fd68b6dee15709e1
Ivan <adamovskij00@mail.ru>
2022-07-26 10:33:52 +0300

    add upd root index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
-import { Provider } from "react-redux"
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 
-import GlobalStyles from "@/globalStyles"
-import store from "@/store"
-import { App } from "@/App"
-import { ErrorBoundary } from "@/components/ErrorBoundary"
-import { ThemeProvider } from "@/components/ThemeProvider"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+
+import GlobalStyles from '@/globalStyles'
+import store from '@/store'
+import { App } from '@/App'
+import { ErrorBoundary } from '@/components/ErrorBoundary'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -21,5 +22,5 @@ ReactDOM.render(
       </BrowserRouter>
     </ThemeProvider>
   </Provider>,
-  document.getElementById("root"),
+  document.getElementById('root'),
 )
