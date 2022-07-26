63654b0e9a8bdd6a387e68f094de5f2359d3aab8
Ivan <adamovskij00@mail.ru>
2022-07-26 08:04:02 +0300

    add action creator refactoring

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,19 @@
-import { ACTIONS } from '@/constants'
+import {
+  ADD_VALUE,
+  CLEAR,
+  SET_ERROR,
+  EVALUATE,
+  SHOW_HISTORY,
+  CLEAR_HISTORY,
+  DELETE_VALUE,
+  THEME_TOGGLE,
+} from '@/constants'
 
-export const actions = {
-  addValue: value => ({ type: ACTIONS.ADD_VALUE, payload: { value } }),
-  deleteLastValue: () => ({ type: ACTIONS.DELETE_VALUE }),
-  clearAll: () => ({ type: ACTIONS.CLEAR }),
-  evaluate: () => ({ type: ACTIONS.EVALUATE }),
-  clearHistory: () => ({ type: ACTIONS.CLEAR_HISTORY }),
-  getShowHistory: value => ({ type: ACTIONS.SHOW_HISTORY, payload: { value } }),
-  setToggleTheme: value => ({ type: ACTIONS.THEME_TOGGLE, payload: { value } }),
-  setError: value => ({ type: ACTIONS.SET_ERROR, payload: { value } }),
-}
+export const addValue = value => ({ type: ADD_VALUE, payload: { value } })
+export const deleteLastValue = () => ({ type: DELETE_VALUE })
+export const clearAll = () => ({ type: CLEAR })
+export const evaluate = () => ({ type: EVALUATE })
+export const clearHistory = () => ({ type: CLEAR_HISTORY })
+export const getShowHistory = value => ({ type: SHOW_HISTORY, payload: { value } })
+export const setToggleTheme = value => ({ type: THEME_TOGGLE, payload: { value } })
+export const setError = value => ({ type: SET_ERROR, payload: { value } })
