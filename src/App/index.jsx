3a480a4a896311229fd0f512c318deda8b15feb5
Ivan <adamovskij00@mail.ru>
2022-07-26 10:37:21 +0300

    add App refactoring

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,32 +1,63 @@
-import React, { lazy, Suspense } from "react"
+import React, { lazy, Suspense, useContext, useEffect } from "react"
 import { Switch, Route } from "react-router-dom"
+import { useDispatch } from "react-redux"
 
-import { Routes } from "@/constants"
-import Loader from "@/components/Loader"
-import PageLayout from "@/pages/PageLayout"
+import { Loader } from "@/components/Loader"
+import { HOME_PAGE_CLASS_ROUTE, HOME_PAGE_ROUTE, SETTINGS_PAGE_ROUTE } from '@/constants'
+import { ThemeContext, ThemeProvider } from "@/components/ThemeProvider"
+import { getLocalStorage } from "@/actions"
 
-const HomePage = lazy(() => import("@/pages/Home"))
-const SettingsPage = lazy(() => import("@/pages/Settings"))
+const HomePage = lazy(() => import("@/pages/Home/index"))
+const SettingsPage = lazy(() => import("@/pages/Settings/index"))
 
-export const App = () => (
-  <Suspense fallback={<Loader />}>
-    <Switch>
-      <Route
-        exact
-        path={Routes.START_PAGE}
-      >
-        <PageLayout />
-      </Route>
-      <Route
-        exact
-        path={Routes.HOME_PAGE}
-        component={HomePage}
-      />
-      <Route
-        exact
-        path={Routes.SETTINGS_PAGE}
-        component={SettingsPage}
-      />
-    </Switch>
-  </Suspense>
-)
+export const Routes = [
+  {
+    id: "home",
+    name: HOME_PAGE_ROUTE,
+    child: HomePage,
+  },
+  {
+    id: "homeClass",
+    name: HOME_PAGE_CLASS_ROUTE,
+    child: HomePage,
+  },
+  {
+    id: "settings",
+    name: SETTINGS_PAGE_ROUTE,
+    child: SettingsPage,
+  },
+]
+
+export const App = () => {
+  const dispatch = useDispatch()
+
+  const { theme } = useContext(ThemeContext)
+
+  useEffect(() => {
+    const data = JSON.parse(
+      localStorage.getItem('redux-store'),
+    )
+    dispatch(getLocalStorage(data?.calculator))
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          {
+            Routes.map(({ id, name, child }) => {
+              return (
+                <Route
+                  key={id}
+                  exact
+                  path={name}
+                  component={child}
+                />
+              )
+            })
+          }
+        </Switch>
+      </Suspense>
+    </ThemeProvider>
+  )
+}
