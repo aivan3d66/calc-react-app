f229a4cfbd24809e790e610aeec91d7912c8dcbb
Ivan <adamovskij00@mail.ru>
2022-07-26 14:33:48 +0300

    add upd KeypadContainer styles

---
+++ src/components/Keypad/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+import theme from '@/theme'
+
+export const KeypadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: ${theme.size.xs}px;
+  justify-content: space-between;
+  padding: 0 ${theme.paddings.p5}rem;
+  width: ${theme.size.max}%;
+`
+
+export const KeypadRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+export const KeypadColumn = styled.div`
+  justify-content: space-between;
+`
