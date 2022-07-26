ef9216dd5fc25976302b907d547d0f2f0f33949e
Ivan <adamovskij00@mail.ru>
2022-07-26 14:33:14 +0300

    add upd Button styles

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -5,20 +5,18 @@ import theme from '@/theme'
 export const ButtonStyled = styled.button`
   background-color: ${props => props.theme.buttonColor.primary};
   border: ${theme.spaces[1]}px solid ${theme.colors.lightGray};
-  border-radius: 30px;
+  border-radius: ${theme.radius.roundedMd}rem;
   color: ${props => props.theme.headerColor.primary};
   cursor: pointer;
   font-size: ${theme.textStyle.xl2.fontSize}rem;
-  height: ${theme.height.h28}rem;
+  height: ${theme.size.max}px;
   line-height: ${theme.textStyle.xl2.lineHeight}rem;
   margin: auto;
-  padding: ${theme.spaces[8]}px;
   transition: 0.3s;
   width: ${theme.size.max}px;
 
   &:hover {
     background-color: ${props => props.theme.buttonHoverColor.primary};
-    border-color: transparent;
     color: ${theme.colors.white};
   }
 
