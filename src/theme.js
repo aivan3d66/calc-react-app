53d491c11cb3cdb54dc4d07cb98cff1c318fdfb6
Ivan <adamovskij00@mail.ru>
2022-07-26 08:36:15 +0300

    add upd theme.js

--- src/theme.js
+++ src/theme.js
@@ -1,40 +1,20 @@
-import { css } from 'styled-components'
+import { css } from "styled-components"
 
-const font = 'Gill Sans, sans-serif'
+const font = "Gill Sans, sans-serif"
 
 // Color palette
-const black = '#000000'
-const darkGray = '#434343'
-const lightGray = '#707070'
-const extraLightGray = '#F2F2F2'
-const white = '#ffffff'
-const error = '#c86464'
-const primary = 'rgb(230, 230, 255)'
-const secondary = '#6c5b7b'
-const secondaryLight = '#6a6b7b'
-
-const appLightTheme = {
-  headerColour: `${darkGray}`,
-  appBackgroundColour: `${white}`,
-  textColour: `${black}`,
-  buttonColour: `${extraLightGray}`,
-}
-const appDarkTheme = {
-  headerColour: `${black}`,
-  appBackgroundColour: `${darkGray}`,
-  textColour: `${primary}`,
-  buttonColour: `${secondaryLight}`,
-}
-const colourTheme = {
-  headerColour: `${secondary}`,
-  appBackgroundColour: `${primary}`,
-  textColour: `${secondaryLight}`,
-  buttonColour: `${extraLightGray}`,
-  buttonHoverColour: `${error}`,
-}
+const black = "#000000"
+const darkGray = "#434343"
+const lightGray = "#707070"
+const extraLightGray = "#F2F2F2"
+const white = "#ffffff"
+const error = "#c86464"
+const lightPink = "#E6E6FFFF"
+const secondary = "#6c5b7b"
+const secondaryLight = "#6a6b7b"
 
 const boxShadows = [
-  'box-shadow: 0px 4px 24px -8px rgba(0,0,0,0.75)',
+  "box-shadow: 0px 4px 24px -8px rgba(0,0,0,0.75)",
 ]
 
 const size = {
@@ -62,24 +42,149 @@ const below = Object.keys(size).reduce((acc, label) => {
   return acc
 }, {})
 
+const theme = {
+  above,
+  below,
+  boxShadows,
+  font,
+  padding: [],
+  spaces: [
+    0,
+    1,
+    2,
+    4,
+    8,
+    10,
+    15,
+    16,
+    19,
+    20,
+    25,
+    32,
+    35,
+    40,
+    64,
+    70,
+    128,
+  ],
+  fontSizes: [
+    12,
+    14,
+    16,
+    20,
+    24,
+    32,
+    40,
+    56,
+    70,
+    72,
+    80,
+    100,
+    500,
+  ],
+  colors: {
+    lightPink,
+    secondary,
+    secondaryLight,
+    black,
+    white,
+    error,
+    darkGray,
+  },
+}
+
+export const appLightTheme = {
+  ...theme,
+  headerColor: {
+    primary: darkGray,
+  },
+  appBackgroundColor: {
+    primary: white,
+  },
+  textColor: {
+    primary: white,
+  },
+  buttonColor: {
+    primary: extraLightGray,
+  },
+  buttonHoverColor: {
+    primary: secondary,
+  },
+}
+export const appDarkTheme = {
+  ...theme,
+  headerColor: {
+    primary: black,
+  },
+  appBackgroundColor: {
+    primary: darkGray,
+  },
+  textColor: {
+    primary: lightPink,
+  },
+  buttonColor: {
+    primary: secondaryLight,
+  },
+  buttonHoverColor: {
+    primary: secondary,
+  },
+}
+export const appColoredTheme = {
+  ...theme,
+  headerColor: {
+    primary: secondary,
+  },
+  appBackgroundColor: {
+    primary: lightPink,
+  },
+  textColor: {
+    primary: secondaryLight,
+  },
+  buttonColor: {
+    primary: extraLightGray,
+  },
+  buttonHoverColor: {
+    primary: error,
+  },
+}
+
 export default {
   above,
   below,
   boxShadows,
   font,
-  spaces: [0, 4, 8, 16, 32, 64, 128],
-  fontSizes: [12, 14, 16, 20, 24, 32, 40, 56, 72, 80],
-  appLightTheme,
-  appDarkTheme,
-  colourTheme,
+  spaces: [
+    0,
+    1,
+    2,
+    4,
+    8,
+    10,
+    15,
+    20,
+    25,
+    // 9
+    16,
+    32,
+    35,
+    40,
+    64,
+    128,
+    19,
+    70,
+    21,
+    170,
+  ],
+  fontSizes: [12, 14, 16, 20, 24, 32, 40, 56, 72, 80, 500],
   colors: {
-    primary,
-    secondary,
-    secondaryLight,
     black,
     darkGray,
     lightGray,
+    extraLightGray,
     white,
     error,
+    lightPink,
+    secondary,
+    secondaryLight,
   },
 }
