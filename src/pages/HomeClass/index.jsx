a44c39dc9300e0229f141d8e61c0dd93ef740c7f
Ivan <adamovskij00@mail.ru>
2022-07-27 10:20:21 +0300

    add HomeClassPage

---
+++ src/pages/HomeClass/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { PageLayout } from '@/containers/PageLayout'
+import CalculatorComponent from '@/containers/Calculator/indexClass'
+
+const HomeClassPage = () => {
+  return (
+    <PageLayout>
+      <CalculatorComponent />
+    </PageLayout>
+  )
+}
+
+export default HomeClassPage
