d20d791ccce3f8a5a21654135124a50543c62764
Ivan <adamovskij00@mail.ru>
2022-07-27 10:38:01 +0300

    add upd buttonValues

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,143 +4,32 @@ export * from './router'
 export const themesOptions = ['Light theme', 'Colored theme', 'Dark theme']
 
 export const buttonValues = [
-  { id: 1, context: 'C', type: 'function' },
-  { id: 2, context: '7', type: 'number' },
-  { id: 3, context: '8', type: 'number' },
-  { id: 4, context: '9', type: 'number' },
-  { id: 5, context: '*', type: 'operation' },
-  { id: 6, context: '-', type: 'operation' },
-  { id: 7, context: '4', type: 'number' },
-  { id: 8, context: '5', type: 'number' },
-  { id: 9, context: '6', type: 'number' },
-  { id: 10, context: '/', type: 'operation' },
-  { id: 11, context: '+', type: 'operation' },
-  { id: 12, context: '1', type: 'number' },
-  { id: 13, context: '2', type: 'number' },
-  { id: 14, context: '3', type: 'number' },
-  { id: 15, context: '=', type: 'operation' },
-  { id: 16, context: '.', type: 'number' },
-  { id: 17, context: '%', type: 'operation' },
-  { id: 18, context: '0', type: 'number' },
-  { id: 19, context: '^', type: 'operation' },
-  { id: 20, context: 'CE', type: 'function' },
+  [
+    { id: 1, value: 'C', type: 'function' },
+    { id: 2, value: '7', type: 'number' },
+    { id: 3, value: '8', type: 'number' },
+    { id: 4, value: '9', type: 'number' },
+    { id: 5, value: '*', type: 'operation' },
+  ],
+  [
+    { id: 6, value: '-', type: 'operation' },
+    { id: 7, value: '4', type: 'number' },
+    { id: 8, value: '5', type: 'number' },
+    { id: 9, value: '6', type: 'number' },
+    { id: 10, value: '/', type: 'operation' },
+  ],
+  [
+    { id: 11, value: '+', type: 'operation' },
+    { id: 12, value: '1', type: 'number' },
+    { id: 13, value: '2', type: 'number' },
+    { id: 14, value: '3', type: 'number' },
+    { id: 15, value: '=', type: 'operation' },
+  ],
+  [
+    { id: 16, value: '.', type: 'number' },
+    { id: 17, value: '%', type: 'operation' },
+    { id: 18, value: '0', type: 'number' },
+    { id: 19, value: '^', type: 'operation' },
+    { id: 20, value: 'CE', type: 'function' },
+  ],
 ]
-
-// export const btnValues = [
-//   {
-//     value: 'C',
-//     dispatchType: CLEAR,
-//   },
-//   {
-//     value: '7',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '8',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '9',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '*',
-//     dispatchType: ADD_VALUE,
-//   },
-//
-//   {
-//     value: '-',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '4',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '5',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '6',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '/',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '+',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '1',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '2',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '3',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '=',
-//     dispatchType: EVALUATE,
-//   },
-//   {
-//     value: '.',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '(',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: '0',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: ')',
-//     dispatchType: ADD_VALUE,
-//   },
-//   {
-//     value: 'CE',
-//     dispatchType: DELETE_VALUE,
-//   },
-//   {
-//     value: '%',
-//     dispatchType: ADD_VALUE,
-//   },
-// ]
-
-// export const buttonValues = [
-//   [
-//     { value: 'C' },
-//     { value: '7' },
-//     { value: '8' },
-//     { value: '9' },
-//     { value: '*' },
-//   ],
-//   [
-//     { value: '-' },
-//     { value: '4' },
-//     { value: '5' },
-//     { value: '6' },
-//     { value: '/' },
-//   ],
-//   [
-//     { value: '+' },
-//     { value: '1' },
-//     { value: '2' },
-//     { value: '3' },
-//     { value: '=' },
-//   ],
-//   [
-//     { value: '.' },
-//     { value: '%' },
-//     { value: '0' },
-//     { value: '+-' },
-//     { value: 'CE' },
-//   ],
-// ]
