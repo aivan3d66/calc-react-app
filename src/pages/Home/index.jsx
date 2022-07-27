23d86fc6dbecf4a6835d360e4970d319c5fcc122
Ivan <adamovskij00@mail.ru>
2022-07-27 08:39:14 +0300

    add upd Homepage

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-import Calculator from '@/containers/Calculator/Calculator'
 import { PageLayout } from '@/containers/PageLayout'
+import CalculatorComponent from '@/containers/Calculator'
 
 const HomePage = () => {
   return (
     <PageLayout>
-      <Calculator />
+      <CalculatorComponent />
     </PageLayout>
   )
 }
