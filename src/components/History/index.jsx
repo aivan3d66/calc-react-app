30d0b317eac5bb9feb31b446be62dae3ec61da97
Ivan <adamovskij00@mail.ru>
2022-07-28 12:16:47 +0300

    add refactor History FC

--- src/components/History/index.jsx
+++ src/components/History/index.jsx
@@ -1,62 +1,24 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
+import { useSelector } from 'react-redux'
 
 import {
   HistoryContainer,
   HistoryTitle,
-  HistoryList,
-  HistoryListItem,
+  HistoryList, HistoryListItem,
 } from '@/components/History/styled'
 import { ThemeContext } from '@/components/ThemeProvider'
-import { getSliceNum } from '@/helpers'
 
 export const History = () => {
   const { theme } = useContext(ThemeContext)
-  const [history, setHistory] = useState(JSON.parse(localStorage.getItem('history')) || [])
-  const [reloadHistory, setReloadHistory] = useState(null)
-
-  useEffect(() => {
-    const reload = setInterval(changeState, 0)
-    setReloadHistory(reload)
-
-    return () => clearInterval(reloadHistory)
-  }, [])
-
-  const changeState = () => {
-    setHistory(JSON.parse(localStorage.getItem('history')))
-  }
-
-  const getHistory = history => {
-    let res = []
-    if (history) {
-      res = history.map(({ firstValue, operator, memory }, i) => {
-        return (
-          <HistoryListItem key={i}>
-            {
-              `${getSliceNum(firstValue)}
-              ${operator}
-              ${getSliceNum(memory)}`
-            }
-          </HistoryListItem>
-        )
-      })
-    }
-    return res
-  }
-
-  const operationsHistory = getHistory(history)
+  const history = useSelector(state => state.appReducer.historyList)
 
   return (
     <HistoryContainer id="historyContainer" theme={theme}>
       <HistoryTitle>History</HistoryTitle>
       <HistoryList>
-        {operationsHistory}
+        {history.map((element, ind) => <HistoryListItem key={ind}>{element}</HistoryListItem>)}
       </HistoryList>
-      {/* <ControlPanel showHistory={showHistory}/> */}
     </HistoryContainer>
   )
 }
 
-// History.propTypes = {
-//   historyList: PropTypes.array,
-//   showHistory: PropTypes.bool,
-// }
