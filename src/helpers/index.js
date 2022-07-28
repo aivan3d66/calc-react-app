ab33d43fa5854c57bcc71e2e6336ef0eaa6a0118
Ivan <adamovskij00@mail.ru>
2022-07-28 12:15:52 +0300

    add upd helpers

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,166 +1,55 @@
-export const getRoundedNum = str => {
-  return str.length > 1 ? str.slice(0, -1) : '0'
+export const expressionHelper = (temp, isAnswer, reAnswer, expression, dispatch, writePad, equals) => {
+  const operatorsString = '/*-+%.'
+  const numbersString = '0123456789'
+
+  switch (operatorsString.includes(temp)) {
+    case true: {
+      inputOperation(operatorsString, expression, temp, isAnswer, reAnswer, dispatch, writePad)
+      break
+    }
+    case false: {
+      inputNumber(numbersString, isAnswer, dispatch, equals, writePad, reAnswer, temp)
+      break
+    }
+    default: {
+      return 0
+    }
+  }
+}
+
+export const inputDot = (expression, dispatch, writePad, temp) => {
+  const tempExpression = `${expression}.`
+  if (/\d+\.\d+\.|\d+\.\./.test(tempExpression)) {
+    alert('Expression is wrong!')
+  } else dispatch(writePad(`${temp}`))
+}
+
+export const inputNumber = (numbersString, isAnswer, dispatch, equals, writePad, reAnswer, temp) => {
+  if (numbersString.includes(temp)) {
+    if (isAnswer) {
+      dispatch(equals(temp))
+      reAnswer()
+    } else dispatch(writePad(temp))
+  } else {
+    dispatch(writePad(` ${temp} `))
+  }
+}
+
+export const inputOperation = (operatorsString, expression, temp, isAnswer, reAnswer, dispatch, writePad) => {
+  if (temp !== '.') {
+    if ((operatorsString.includes(expression[expression.length - 2]) ||
+        operatorsString.includes(expression[expression.length - 1])) &&
+      expression[expression.length - 2] !== '.') {
+      alert('Wrong expression1')
+    } else {
+      if (isAnswer) {
+        reAnswer()
+      }
+      dispatch(writePad(` ${temp} `))
+    }
+  } else {
+    inputDot(expression, dispatch, writePad, temp)
+  }
 }
 
 export const getSliceNum = num => parseFloat(num.toFixed(2))
-
-export const control = testString => {
-  testString = testString.replace(/\++/g, '+')
-  testString = testString.replace(/--/g, '+')
-  testString = testString.replace(/\.\./g, '.')
-  testString = testString.replace(/[+-][-+]/g, '-')
-  return testString
-}
-
-export const calculation = buffString => {
-  buffString = buffString.replace(/([^[0-9.]{1})/g, ' $1 ').trim()
-  buffString = buffString.replace(/ {1,}/g, ' ')
-  const buffArray = buffString.split(/\s/)
-  const polishString = []
-  const polishStack = []
-  let stringId = -1
-  let stackId = -1
-
-  const unaryMinus = buffArray[0] === '-'
-  if (unaryMinus) {
-    buffArray.shift()
-  }
-
-  for (let i = 0; i < buffArray.length; i++) {
-    switch (buffArray[i]) {
-      case '(':
-        stackId++
-        polishStack[stackId] = buffArray[i]
-        break
-      case ')':
-        while (stackId >= 0 && polishStack[stackId] !== '(') {
-          stringId++
-          polishString[stringId] = polishStack[stackId]
-          stackId--
-        }
-        stackId--
-        break
-      case '+':
-        while (stackId >= 0 && (polishStack[stackId] === '+' ||
-          polishStack[stackId] === '-' ||
-          polishStack[stackId] === '*' ||
-          polishStack[stackId] === '/')) {
-          stringId++
-          polishString[stringId] = polishStack[stackId]
-          stackId--
-        }
-        stackId++
-        polishStack[stackId] = buffArray[i]
-        break
-      case '-':
-        while (stackId >= 0 && (polishStack[stackId] === '+' ||
-          polishStack[stackId] === '-' ||
-          polishStack[stackId] === '*' ||
-          polishStack[stackId] === '/')) {
-          stringId++
-          polishString[stringId] = polishStack[stackId]
-          stackId--
-        }
-        stackId++
-        polishStack[stackId] = buffArray[i]
-        break
-      case '*':
-        while (stackId >= 0 && (polishStack[stackId] === '*' || polishStack[stackId] === '/')) {
-          stringId++
-          polishString[stringId] = polishStack[stackId]
-          stackId--
-        }
-        stackId++
-        polishStack[stackId] = buffArray[i]
-        break
-      case '/':
-        while (stackId >= 0 && (polishStack[stackId] === '*' || polishStack[stackId] === '/')) {
-          stringId++
-          polishString[stringId] = polishStack[stackId]
-          stackId--
-        }
-        stackId++
-        polishStack[stackId] = buffArray[i]
-        break
-      case '%':
-        while (stackId >= 0 && (polishStack[stackId] === '%' ||
-          polishStack[stackId] === '-' ||
-          polishStack[stackId] === '*' ||
-          polishStack[stackId] === '/')) {
-          stringId++
-          polishString[stringId] = polishStack[stackId]
-          stackId--
-        }
-        stackId++
-        polishStack[stackId] = buffArray[i]
-        break
-      default:
-        stringId++
-        polishString[stringId] = buffArray[i]
-    }
-  }
-  while (stackId >= 0) {
-    stringId++
-    polishString[stringId] = polishStack[stackId]
-    stackId--
-  }
-  stackId = -1
-  const stringIdMax = stringId
-
-  let int
-  let res
-
-  for (stringId = 0; stringId <= stringIdMax; stringId++) {
-    if (unaryMinus) {
-      switch (polishString[stringId]) {
-        case '+':
-          stackId--
-          polishStack[stackId] = polishStack[stackId] - polishStack[stackId + 1]
-          break
-        case '-':
-          stackId--
-          polishStack[stackId] = polishStack[stackId] + polishStack[stackId + 1]
-          break
-        default:
-          stackId++
-          polishStack[stackId] = parseFloat(polishString[stringId])
-      }
-    } else {
-      switch (polishString[stringId]) {
-        case '+':
-          stackId--
-          polishStack[stackId] = polishStack[stackId] + polishStack[stackId + 1]
-          break
-        case '-':
-          stackId--
-          polishStack[stackId] = polishStack[stackId] - polishStack[stackId + 1]
-          break
-        case '*':
-          stackId--
-          polishStack[stackId] = polishStack[stackId] * polishStack[stackId + 1]
-          break
-        case '/':
-          stackId--
-          polishStack[stackId] = polishStack[stackId] / polishStack[stackId + 1]
-          break
-        case '%':
-          stackId--
-          int = Math.floor(polishStack[stackId] / polishStack[stackId + 1])
-          res = int * 2
-          polishStack[stackId] = polishStack[stackId] - res
-          break
-        default:
-          stackId++
-          polishStack[stackId] = parseFloat(polishString[stringId])
-      }
-    }
-  }
-  return unaryMinus ? -polishStack[stackId] : polishStack[stackId]
-}
-
-export const getNormalizeNumber = x => {
-  if (x.toString().includes('.')) {
-    return x.toPrecision(2)
-  }
-  return x
-}
