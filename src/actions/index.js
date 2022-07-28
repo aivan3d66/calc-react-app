09b336920441bf5e60a68f029d06386d08fc0d9a
Ivan <adamovskij00@mail.ru>
2022-07-28 12:27:09 +0300

    add upd action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,23 +1,21 @@
 import {
   ADD_VALUE,
-  CLEAR,
   SET_ERROR,
   EVALUATE,
   SHOW_HISTORY,
   CLEAR_HISTORY,
   DELETE_VALUE,
-  THEME_TOGGLE,
   GET_LOCAL_STORE,
   TOGGLE_THEME,
-} from "@/constants"
+  ADD_TO_HISTORY,
+} from '@/constants'
 
 export const addValue = value => ({ type: ADD_VALUE, payload: { value } })
-export const deleteLastValue = () => ({ type: DELETE_VALUE })
-export const clearAll = () => ({ type: CLEAR })
-export const evaluate = () => ({ type: EVALUATE })
+export const deleteValue = () => ({ type: DELETE_VALUE })
+export const evaluate = value => ({ type: EVALUATE, payload: { value } })
+export const addToHistory = value => ({ type: ADD_TO_HISTORY, payload: { value } })
 export const clearHistory = () => ({ type: CLEAR_HISTORY })
 export const getShowHistory = value => ({ type: SHOW_HISTORY, payload: { value } })
-export const setToggleTheme = value => ({ type: THEME_TOGGLE, payload: { value } })
 export const setError = value => ({ type: SET_ERROR, payload: { value } })
 export const toggleTheme = value => ({ type: TOGGLE_THEME, payload: { value } })
 export const getLocalStorage = value => ({ type: GET_LOCAL_STORE, payload: { value } })
