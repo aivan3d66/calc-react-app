a70da87b9d0c24a450b5857df9269409a442c5b9
Ivan <adamovskij00@mail.ru>
2022-07-29 10:52:55 +0300

    add refactor KeypadContainer class component

--- src/containers/Keypad/indexClass.jsx
+++ src/containers/Keypad/indexClass.jsx
@@ -1,30 +1,70 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-import { KeypadColumn, KeypadContainer, KeypadRow } from '@/containers/Keypad/styled'
-import { buttonValues } from '@/constants'
-import Button from '@/components/Button/indexClass'
+import { addToHistory, addValue, deleteValue, evaluate, setError } from '@/actions'
+import { calculation } from '@/utils'
+import { expressionHelper } from '@/helpers'
+import Keypad from '@/containers/Keypad/KeypadClass/indexClass'
 
-export default class Keypad extends React.Component {
+class KeypadContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      answer: false,
+    }
+  }
+
+  getIsAnswer() {
+    this.setState((state, props) => ({
+      answer: state.answer === false,
+    }))
+  }
+
   render() {
-    return (
-      <KeypadContainer id="keypadContainer">
-        {
-          buttonValues.map((rows, index) => (
-            <KeypadRow key={index}>
-              {rows.map(({id, value, type}) => (
-                <KeypadColumn key={value}>
-                  <Button
-                    key={id}
-                    value={value}
-                    type={type}
-                    onKeyClick={this.props.onKeyClick}
-                  />
-                </KeypadColumn>
-              ))}
-            </KeypadRow>
-          ))
-        }
-      </KeypadContainer>
-    )
+    return <Keypad onKeyClick={this.props.onKeyClick} expressionValue={this.props.expressionValue} />
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    expressionValue: state.appReducer.expression,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onKeyClick: (event, expression) => {
+      if (event.target.classList.contains('mainKeypad')) return
+      switch (event.target.textContent) {
+        case 'CE': {
+          dispatch(deleteValue())
+          break
+        }
+        case 'C': {
+          dispatch(evaluate(expression.slice(0, expression.length - 1)))
+          break
+        }
+        case '=': {
+          try {
+            const temp = expression
+            const answer = calculation(expression.toString()).toString()
+            const t = dispatch(evaluate(answer))
+            dispatch(addToHistory(`${temp} = ${t.payload.value}`))
+            this.props.getIsAnswer()
+          } catch (error) {
+            dispatch(setError(error.message))
+          }
+          break
+        }
+        default: {
+          const temp = event.target.textContent
+
+          expressionHelper(temp, false, false, expression, dispatch, addValue, evaluate)
+        }
+      }
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(KeypadContainer)
+
