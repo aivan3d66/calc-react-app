f3d526b2dd07e8e61da462339d1aa6c3563a82a4
Ivan <adamovskij00@mail.ru>
2022-07-29 10:07:55 +0300

    add refactor History class component

--- src/components/History/indexClass.jsx
+++ src/components/History/indexClass.jsx
@@ -1,60 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-import { HistoryContainer, HistoryList, HistoryListItem, HistoryTitle } from '@/components/History/styled'
-import { getSliceNum } from '@/helpers'
+import { HistoryContainer, HistoryList, HistoryTitle } from '@/components/History/styled'
 import { ThemeContext } from '@/components/ThemeProvider'
 
+
 export default class History extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      history: JSON.parse(localStorage.getItem('history')) || [],
-      reloadHistory: null,
-    }
-  }
-
-  componentDidMount() {
-    const reload = setInterval(this.changeState, 0)
-    this.setState({ reloadHistory: reload })
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.state.reloadHistory)
-  }
-
-  changeState = () => {
-    this.setState({
-      history: JSON.parse(localStorage.getItem('history')),
-    })
-  }
-
-  getHistory = history => {
-    let res = []
-    if (history) {
-      res = history.map(({ firstValue, operator, memory }, i) => {
-        return (
-          <HistoryListItem key={i}>
-            {
-              `${getSliceNum(firstValue)}
-              ${operator}
-              ${getSliceNum(memory)}`
-            }
-          </HistoryListItem>
-        )
-      })
-    }
-    return res
-  }
-
   render() {
-    const operationsHistory = this.getHistory(this.state.history)
     return (
       <ThemeContext.Consumer>
         {({ theme }) => (
           <HistoryContainer id="historyContainer" theme={theme}>
             <HistoryTitle>History</HistoryTitle>
             <HistoryList>
-              {operationsHistory}
+              {this.props.history}
             </HistoryList>
             {/* <ControlPanel showHistory={showHistory}/> */}
           </HistoryContainer>
@@ -63,4 +22,8 @@ export default class History extends React.Component {
 
     )
   }
+}
+
+History.propTypes = {
+  history: PropTypes.array,
 }
