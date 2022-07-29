f421c4826686f88e14e8cc9169ee82a1ced44a18
Ivan <adamovskij00@mail.ru>
2022-07-29 15:42:38 +0300

    add changeOperator action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,11 +7,15 @@ import {
   DELETE_VALUE,
   TOGGLE_THEME,
   ADD_TO_HISTORY,
+  OPERATOR_CHANGE,
+  ADD_TO_EXPRESSION_HISTORY,
 } from '@/constants'
 
 export const addValue = value => ({ type: ADD_VALUE, payload: { value } })
 export const deleteValue = () => ({ type: DELETE_VALUE })
 export const evaluate = value => ({ type: EVALUATE, payload: { value } })
+export const addToExpressionHistory = value => ({ type: ADD_TO_EXPRESSION_HISTORY, payload: { value } })
+export const changeOperator = value => ({ type: OPERATOR_CHANGE, payload: { value } })
 export const addToHistory = value => ({ type: ADD_TO_HISTORY, payload: { value } })
 export const clearHistory = () => ({ type: CLEAR_HISTORY })
 export const getShowHistory = value => ({ type: SHOW_HISTORY, payload: { value } })
