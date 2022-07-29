ce1c33b7ab8947284a73d156a8eeb5fc0b5122dd
Ivan <adamovskij00@mail.ru>
2022-07-29 15:43:39 +0300

    add upd display styles

--- src/components/Display/styled.js
+++ src/components/Display/styled.js
@@ -5,10 +5,19 @@ import theme from '@/theme'
 export const DisplayContainer = styled.div`
   border-bottom: ${theme.spaces[2]}px solid ${theme.colors.darkGray};
   color: ${props => props.theme.headerColor.primary};
+  display: flex;
   font-size: ${theme.textStyle.xl2.fontSize}rem;
+  justify-content: space-between;
   line-height: ${theme.textStyle.xl2.lineHeight}rem;
   margin: 0 20px 30px 0;
   overflow: hidden;
   padding: ${theme.paddings.p10}rem 0;
   text-align: right;
 `
+
+export const DisplayHistory = styled.div`
+  color: ${theme.colors.secondaryLight};
+  font-size: ${theme.textStyle.lg.fontSize}rem;
+`
+
+export const DisplayCurrent = styled.div``
